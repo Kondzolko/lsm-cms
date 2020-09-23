7748e0342e8834c4136c20432e58f214f24f4553
Andriy Kondzolko <kondzolko@gmail.com>
2020-09-24 00:30:52 +0300

    Setup basic UW layout Add fontawesome-free

--- app/controllers/devise/deligated_authorities/sessions_controller.rb
+++ app/controllers/devise/deligated_authorities/sessions_controller.rb
@@ -25,4 +25,9 @@ class Devise::DeligatedAuthorities::SessionsController < Devise::SessionsControl
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  private
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || da_root_path
+  end
 end
--- app/controllers/devise/underwriters/sessions_controller.rb
+++ app/controllers/devise/underwriters/sessions_controller.rb
@@ -25,4 +25,9 @@ class Devise::Underwriters::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  private
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || uw_root_path
+  end
 end
