010aa2cc627844384f04134309c247cfe2de6b20
Andriy Kondzolko <kondzolko@gmail.com>
2020-09-28 17:03:29 -0400

    fixed bug with devise

--- app/models/underwriter.rb
+++ app/models/underwriter.rb
@@ -1,7 +1,7 @@
 class Underwriter < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, # :recoverable, # :registerable,
+  devise :database_authenticatable, :confirmable, # :recoverable, # :registerable,
          :rememberable, :validatable, :trackable
 
   has_many :deals
